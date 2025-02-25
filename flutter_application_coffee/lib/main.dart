ec9da72ed5a7a08df44b200712dab2746eee03c8
Maxim <mortischrzn@gmail.com>
2025-02-25 21:13:08 +0300

    Initial commit

---
+++ flutter_application_coffee/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            alignment: Alignment.center,
+            child: Text(
+            'dfdfh'
+            ),
+            color: Colors.deepOrange,
+          ),
+        ),
+      ),
+    )
+  );
+}

db9228cdca17cb240697e307727dc8efbde2789d
Maxim <mortischrzn@gmail.com>
2025-02-27 22:31:18 +0300

    Добавлена горизонтальная прокрутка в ленте

--- flutter_application_coffee/lib/pages/home_page/home_interface/scroll_menu_horizontal.dart
+++ flutter_application_coffee/lib/pages/home_page/home_interface/scroll_menu_horizontal.dart
@@ -1,35 +1,73 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_application_coffee/pages/home_page/home_page.dart';
 
-class ScrollMenuHorizontal extends StatelessWidget {
+class ScrollMenuHorizontal extends StatefulWidget {
   const ScrollMenuHorizontal({super.key});
+
+  @override
+  State<ScrollMenuHorizontal> createState() => _ScrollMenuHorizontalState();
+}
+class _ScrollMenuHorizontalState extends State<ScrollMenuHorizontal>{
+
+  final List<Map<String, dynamic>> coffeeType = [
+    {"type": "Espresso", "isSelected": true},
+    {"type": "Latte", "isSelected": false},
+    {"type": "Cappuccino", "isSelected": false},
+    {"type": "Americano", "isSelected": false},
+    {"type": "Mocha", "isSelected": false},
+  ];
+
+  void _coffeeSelectedType(int selectedIndex){
+    setState(() {
+      for(var element in coffeeType){
+        element['isSelected'] = false;
+      }
+      coffeeType[selectedIndex]['isSelected'] = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 40,
-      child: ListView(
+      child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        children: [
-          _buildItem('Cappuchino'),
-          _buildItem('Espresso'),
-          _buildItem('Latte'),
-          _buildItem('Flet White'),
-          _buildItem('Raf'),
-        ],
-      ),
+        itemCount: coffeeType.length,
+        itemBuilder: (context, index){
+        return CoffeeType(
+          coffeeType: coffeeType[index]['type'],
+          isSelected: coffeeType[index]['isSelected'],
+          onTap: () => _coffeeSelectedType(index)
+        );
+      })
     );
   }
 }
 
-Widget _buildItem(String text){
-  return Container(
-    //width: 120,
-    margin: const EdgeInsets.only(left: 18),
-    child: Center(
-      child: Text(
-        text,
-        style: const TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.bold),
+class CoffeeType extends StatelessWidget{
+  final String coffeeType;
+  final bool isSelected;
+  final VoidCallback onTap; 
+
+  const CoffeeType({super.key, required this.coffeeType, required this.isSelected, required this.onTap});
+  
+  @override
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: onTap,
+    child: Container(
+      margin: const EdgeInsets.only(left: 18),
+        child: Center(
+          child: Text(
+            coffeeType,
+            style: TextStyle(
+            color: isSelected ? Color.fromARGB(255, 198, 124, 78) : Colors.grey, 
+            fontSize: 18, 
+            fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
